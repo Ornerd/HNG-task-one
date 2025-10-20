3e7b36b560cd474919ea1c8d72005a9046759315
Ornerd <ukoorbytal@gmail.com>
2025-10-20 23:35:30 +0100

    finsal commit before submission for stage 1

--- myForm.js
+++ myForm.js
@@ -2,15 +2,19 @@
 
 const myForm = document.querySelector('form');
 
-const fullName = document.getElementById('name');
+// const fullName = document.getElementById('name');
 
 
 function validateFields(field) {
+
+    const errorElement = field.parentElement.querySelector('span');
+
     if(!field.validity.valid) {
-        console.log(`is not valid`);
+        errorElement.textContent = field.dataset.error || "This field is required";
         return false;
     }
 
+    errorElement.textContent = "";
     console.log(`is valid`);
     return true;
 }
@@ -19,9 +23,30 @@ function validateFields(field) {
 myForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const isValid = validateFields(fullName);
+    const fieldsToValidate = document.querySelectorAll('input, textarea');
 
-    isValid ? console.log('Form submitted') : console.log('Form not submitted');
+
+    let isValid = true;
+
+    fieldsToValidate.forEach((field) => {
+        const valid = validateFields(field);
+
+        valid? isValid = true : isValid= false;
+    })
+
+   
+
+    if (isValid) {
+        myForm.reset();
+
+        const successMessage = document.getElementById('success-message');
+
+        successMessage.style.display = 'block';
+
+        setTimeout(() => {
+            successMessage.style.display = 'none';
+        }, 5000);
+    }
 
 
 })
